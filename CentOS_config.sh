1b98dd4b54426fb2892eec7d56d0d9408d4725b7
Evan Bolt <boltevan7@gmail.com>
2019-11-24 10:28:52 -0500

    Update CentOS_config.sh

--- CentOS_config.sh
+++ CentOS_config.sh
@@ -31,3 +31,13 @@ sed -i 's/enabled=0/enabled=1/4' /etc/yum.repos.d/elrepo.repo
 
 # Require root password for sudo
 echo "Defaults rootpw" > /etc/sudoers.d/Require_Root_Pass
+
+# Modify nano, CentOS 7 has an older version so some options arent'y the same
+if [[ $(cat /etc/centos-release | cut -d ' ' -f 4 | cut -d '.' -f 1) -ge 8 ]]
+then
+  sed -i 's/# set smooth/set smooth/' /etc/nanorc
+  sed -i 's/# set constantshow/set constantshow/' /etc/nanorc
+else
+  sed -i 's/# set smooth/set smooth/' /etc/nanorc
+  sed -i 's/# set const/set const/' /etc/nanorc
+fi
