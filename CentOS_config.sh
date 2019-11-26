1fd37bf5a4c3df1c9e46024d20e165dd2fed89d7
Evan Bolt <boltevan7@gmail.com>
2019-11-25 20:14:33 -0500

    Update CentOS_config.sh

--- CentOS_config.sh
+++ CentOS_config.sh
@@ -2,32 +2,37 @@
 
 if  (( EUID != 0 ))
 then
-	echo "Script should be run as root"
-	exit 1
+        echo "Script should be run as root"
+        exit 1
 else
-	echo "Detected running as root"
+        echo "Detected running as root"
 fi
 
 if [[ $(cat /etc/centos-release | cut -d ' ' -f 4 | cut -d '.' -f 1) -ge 8 ]]
 then
   CENTVER="8+"
-  PACKAGE_MANGER="dnf"
+  ELREPO="https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm"
+  PACKAGE_MANAGER="dnf"
 else
   CENTVER="7"
+  ELREPO="https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm"
   PACKAGE_MANAGER="yum"
 fi
-
+echo ${PACKAGE_MANAGER}
 # Configure shell
 wget -O /etc/profile.d/colorprompt.sh https://gist.githubusercontent.com/kholis/3985921/raw/d55393f64c4a21dbd419c769548c6319a21d84b4/colorprompt.sh
 
-# Enable centosplus
-sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-centosplus.repo
-
-# Enable PowerTools
-sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-PowerTools.repo
-if [[ -f /etc/yum.repos.d/CentOS-Stream-PowerTools.repo ]]
+if [[ CENTVER == "8+" ]]
 then
-	sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-Stream-PowerTools.repo
+  # Enable centosplus
+  sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-centosplus.repo
+
+  # Enable PowerTools
+  sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-PowerTools.repo
+  if [[ -f /etc/yum.repos.d/CentOS-Stream-PowerTools.repo ]]
+  then
+        sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-Stream-PowerTools.repo
+  fi
 fi
 
 # Install epel
@@ -35,7 +40,8 @@ fi
 
 # Install elrepo
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-"${PACKAGE_MANAGER}" install -y https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm
+
+"${PACKAGE_MANAGER}" install -y "${ELREPO}"
 sed -i 's/enabled=0/enabled=1/4' /etc/yum.repos.d/elrepo.repo
 
 # Require root password for sudo
