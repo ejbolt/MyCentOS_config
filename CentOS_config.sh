85267a9c16ed1efcc7f1ab6212e65a38601e7157
Evan Bolt <boltevan7@gmail.com>
2019-11-25 20:21:15 -0500

    Update CentOS_config.sh

--- CentOS_config.sh
+++ CentOS_config.sh
@@ -58,7 +58,7 @@ else
 fi
 
 # Fix DNF tab completion
-if [[ CENTER == "8+" ]]
+if [[ CENTVER == "8+" ]]
 then
   "${PACKAGE_MANAGER}" install python36
   wget -O /usr/lib/python3/site-packages/dnf/cli/completion_helper.py https://raw.githubusercontent.com/rpm-software-management/dnf/master/dnf/cli/completion_helper.py.in
